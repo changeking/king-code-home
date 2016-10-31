c52dd64d0552314b5630db44f74b7d50800b8713
hongcheche <731779554@qq.com>
2016-10-31 17:12:08 +0800

    add ziduan

--- test.java
+++ test.java
@@ -1,11 +1,16 @@
 public class test{
 	private String name;
 	private int age;
+	private String id
 	public test(){
 
 	}
 	public test(String name,int age){
 		this.name=name;
 		this.age=age;
+	}
+
+	public void run(){
+		syso(dadasd)
 	}
 }
